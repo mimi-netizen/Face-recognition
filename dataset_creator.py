8abafeb7a023b93a2690f2c37ff7ba66dd086883
Celyne <dianakydd2@gmail.com>
2024-10-03 20:52:51 +0300

    image converter

--- dataset_creator.py
+++ dataset_creator.py
@@ -31,4 +31,10 @@ insertorupdate(Name,Id,age)
 #detect face in web camera
 sampleNum=0
 while(True):
-    ret,img=
+    ret,img=cam.read();
+    gray=cv2.cvtColor(img.cv2.COLOR_BGR2GRAY)           #Image convert into BGRGRAY color
+    faces=faceDetect.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        sampleNum=sampleNum=1;
+        cv2.imwrite("dataset/user."=str(Id)+"."+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
